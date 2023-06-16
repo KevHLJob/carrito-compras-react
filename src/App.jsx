1c53360dd5e218bc5b2f819b375808c3ef7d364e
KevHLJob <KevHLJob@github.com>
2023-06-15 23:37:07 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import {products} from './mocks/products.json'
+import { Products } from './components/Products.jsx'
+
+
+
+function App () {
+
+  return (
+      <Products products={products} />
+     
+  )
+}
+
+export default App
